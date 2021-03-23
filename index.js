a13198cad7b5d5525fcb1277044633db3e9d7dfb
Juli_324 <juja2769@yahoo.de>
2021-03-23 10:17:52 +0100

    added second request method if skybot is online

--- index.js
+++ index.js
@@ -1,24 +1,51 @@
 const Discord = require('discord.js');
-const downdetector = new Discord.Client({ ws: { intents: Discord.Intents.ALL }});
-const skybot = new Discord.Client({ shards: 'auto'});
+const downdetector = new Discord.Client({ws: {intents: Discord.Intents.ALL}});
+const skybot = new Discord.Client({shards: 'auto'});
 require('dotenv').config()
 
 downdetector.on('ready', () => {
-  console.log(`Logged in as ${downdetector.user.tag}!`);
-  downdetector.guilds.fetch('636937988248436736')
+    console.log(`Logged in as ${downdetector.user.tag}!`);
+    downdetector.guilds.fetch('636937988248436736');
 });
 skybot.on('ready', () => {
     console.log(`Logged in as ${skybot.user.tag}!`);
-  });
+});
+
+async function isOffline() {
+    let guild = downdetector.guilds.cache.get('636937988248436736');
+    let channel = guild.channels.cache.get('823219739572437052');
+    await channel.send('Skybot is offline <@&667609814079111226>');
+    await skybot.user.setPresence({activity: {name: 'CURRENTLY UNAVAILABLE!'}, status: 'dnd'});
+    console.log("Bot offline")
+}
+
 
 downdetector.on('presenceUpdate', async (oldPresence, newPresence) => {
-    if (newPresence.userID === '630106665387032576' && newPresence.status === 'offline') {
-        guild = downdetector.guilds.cache.get('636937988248436736')
-        channel = guild.channels.cache.get('823219739572437052')
-        await channel.send('Skybot is offline <@&667609814079111226>')
-        await skybot.user.setPresence({ activity: { name: 'CURRENTLY UNAVAILABLE!'}, status: 'dnd'})
+    if (newPresence.userID === '630106665387032576' &&
+        newPresence.status === 'offline') {
+        isOffline();
     }
 })
 
+
+let lastSentTime = 0;
+let isOK = true;
+
+setInterval(function () {
+    if (!isOK) {
+        isOffline();
+    }
+    downdetector.users.cache.get("630106665387032576").send("!isOnline");
+    lastSentTime = Math.floor(new Date() / 1000);
+    isOK = false;
+}, 60000);
+
+downdetector.on("message", (msg) => {
+    if (msg.author.id === "630106665387032576" &&
+        Math.floor(new Date() / 1000) < lastSentTime + 10) {
+        isOK = true;
+    }
+});
+
 downdetector.login(process.env.downdetector_token);
-skybot.login(process.env.skybot_token)
+skybot.login(process.env.skybot_token);
